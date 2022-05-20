8b4491b94daa751c5b9fc937b54d576c5ee6eb56
ale-lit <zeldor@ya.ru>
2022-05-20 16:16:40 +0300

    feat: add CORS control

--- controllers/BaseController.php
+++ controllers/BaseController.php
@@ -10,6 +10,18 @@ abstract class BaseController
     {
         header('HTTP/1.1 200 OK');
         header('Content-type: application/json');
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Credentials: true');
+        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+        header('Access-Control-Allow-Headers: Authorization, X-Requested-With, Content-Type, Accept, Origin');
+
+        echo json_encode($this->answer, JSON_UNESCAPED_UNICODE);
+    }
+
+    protected function sendCreated()
+    {
+        header('HTTP/1.1 201 Created');
+        header('Content-type: application/json');
         echo json_encode($this->answer, JSON_UNESCAPED_UNICODE);
     }
 
@@ -23,13 +35,31 @@ abstract class BaseController
         header('HTTP/1.1 405 Not Allowed');
     }
 
-    protected function showBadRequest()
+    protected function showBadRequest($message = false)
     {
         header('HTTP/1.1 400 Bad Request');
+        if ($message) {
+            $this->answer = [ "message" => $message ];
+            header('Content-type: application/json');
+            echo json_encode($this->answer, JSON_UNESCAPED_UNICODE);
+        }
     }
 
-    protected function showNotAuthorized()
+    protected function showConflictError()
+    {
+        header('HTTP/1.1 409 Conflict');
+        $this->answer = [ "message" => "Указанные данные уже есть в базе" ];
+        header('Content-type: application/json');
+        echo json_encode($this->answer, JSON_UNESCAPED_UNICODE);
+    }
+
+    protected function showNotAuthorized($message = false)
     {
         header('HTTP/1.1 401 Unauthorized');
+        if ($message) {
+            $this->answer = [ "message" => $message ];
+            header('Content-type: application/json');
+            echo json_encode($this->answer, JSON_UNESCAPED_UNICODE);
+        }
     }
 }
